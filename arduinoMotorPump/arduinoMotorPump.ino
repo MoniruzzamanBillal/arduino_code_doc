7910085c3f6024c41f5f5c0e38c8625792998dc9
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-07 16:41:39 +0600

    ultrasonic sensor config with motor , and relay , turn on motor based on condition

--- arduinoMotorPump/arduinoMotorPump.ino
+++ arduinoMotorPump/arduinoMotorPump.ino
@@ -1,29 +1,31 @@
 
+
 #include <LiquidCrystal.h>
 
 // LiquidCrystal lcd(rs, e, d4, d5, d6, d7)
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
-
-
 int trigPin = 8;
 int echoPin = 9;
-int buzzer = 10;
+int relaypin = 10;
 float distance;
 float duration;
 
+// relay_pin low = on 
 
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(buzzer, OUTPUT);
+  pinMode(relaypin, OUTPUT);
   Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.clear();
 }
 
+
 void loop() {
+
 
 
   digitalWrite(trigPin, LOW);
@@ -36,17 +38,16 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = (duration / 2) * 0.034;
 
-  
+
   lcd.setCursor(0, 0);
   lcd.print("dist = " + String(distance) + " cm");
 
 
   if (distance <= 30) {
-    digitalWrite(buzzer, HIGH);
+    digitalWrite(relaypin, LOW);
   } else {
-    digitalWrite(buzzer, LOW);
+    digitalWrite(relaypin, HIGH);
   }
-
 
   delay(500);
 }
