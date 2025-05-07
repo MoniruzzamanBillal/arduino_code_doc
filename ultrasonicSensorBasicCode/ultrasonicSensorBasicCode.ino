1541346a38856808ed27eae1d5b228185647d3a0
Md. Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2025-05-07 12:33:32 +0600

    ultrasonic sensor basic distance code

---
+++ ultrasonicSensorBasicCode/ultrasonicSensorBasicCode.ino
@@ -0,0 +1,36 @@
+
+int trigPin = A0;
+int echoPin = A1;
+float distance;
+float duration;
+
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  digitalWrite(trigPin, LOW);
+  delay(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) * 0.034;
+
+
+  Serial.print("duration = ");
+  Serial.print(duration);
+  Serial.print(" Mili Sec. ");
+  Serial.println("");
+
+  Serial.print("distance  = ");
+  Serial.print(distance);
+  Serial.print(" cm ");
+ Serial.println("");
+  delay(500);
+}
